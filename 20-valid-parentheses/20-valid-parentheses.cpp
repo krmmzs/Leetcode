0488514a3898309b3f11a56d228a566648014199
mouzaisi <69446758+mouzaisibyl@users.noreply.github.com>
2022-06-13 17:40:25 +0800

    Time: 4 ms (31.14%), Space: 6.4 MB (10.20%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> stk;
+        for (auto c : s)
+        {
+            if (c == '(' || c == '{' || c == '[') stk.push(c);
+            else
+            {
+                if (stk.size() && abs(stk.top() - c) <= 2) stk.pop();
+                else return false;
+            }
+        }
+        return stk.empty();
+    }
+};
